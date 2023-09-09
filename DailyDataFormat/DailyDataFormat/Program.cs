7294ba3052f7f5dbd2afc6108086a6f20490337e
Sourav <souravmondals@users.noreply.github.com>
2023-09-09 16:02:05 +0530

    Update Program.cs

--- DailyDataFormat/DailyDataFormat/Program.cs
+++ DailyDataFormat/DailyDataFormat/Program.cs
@@ -1,9 +1,10 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 
 namespace DailyDataFormat
 {
@@ -11,18 +12,23 @@ namespace DailyDataFormat
     {
         public static string filedate;
         public static List<string> StockList;
+        public static List<string> SkipList;
 
         public static List<string> Notfounddata = new List<string>();
         public static List<Datamodel> Newdata = new List<Datamodel>();
 
         public static void Main(string[] args)
         {
-           StockList = File.ReadAllLines("sample.txt")                                           
-                                           .Select(v => getStockList(v))
-                                           .ToList();
+            StockList = File.ReadAllLines("sample.txt")
+                                            .Select(v => getStockList(v))
+                                            .ToList();
+            SkipList = File.ReadAllLines("skiplist.txt")
+                                            .Select(v => getStockList(v))
+                                            .ToList();
 
             DirectoryInfo d = new DirectoryInfo(@".\");
             FileInfo[] Files = d.GetFiles("*.csv");
+           
             foreach (FileInfo file in Files)
             {
                 List<Datamodel> Stockdata = File.ReadAllLines(file.Name)
@@ -32,29 +38,43 @@ namespace DailyDataFormat
                 Stockdata = RemoveDuplicate(Stockdata);
                 foreach (string stock in StockList)
                 {
-                    if (!Stockdata.Any(r=>r.Name==stock))
+                    if (!Stockdata.Any(r => r.Name == stock))
                     {
                         Notfounddata.Add(stock);
-                    }                   
+                    }
+                }
+
+                if (!Directory.Exists("Output"))
+                {
+                    Directory.CreateDirectory("Output");
+                    Directory.CreateDirectory("temp");
                 }
                 
 
                 var lines = new List<string>();
-                var valueLines = Stockdata.Where(row => row.Name!=null).Select(row => string.Join(",", new string[]{ row.Name, row.dateDate, row.Open, row.High, row.Low, row.Close, row.Volume }));
+                var valueLines = Stockdata.Where(row => row.Name != null).Select(row => string.Join(",", new string[] { row.Name, row.dateDate, row.Open, row.High, row.Low, row.Close, row.Volume }));
                 lines.AddRange(valueLines);
-                File.WriteAllLines(filedate+ "-NSE-EQ-F.txt", lines.ToArray());
+                File.WriteAllLines("Output\\" + filedate + "-NSE-EQ-F.txt", lines.ToArray());
 
                 var linesN = new List<string>();
                 var valueLinesN = Newdata.Where(row => row.Name != null).Select(row => string.Join(",", new string[] { row.Name, row.dateDate, row.Open, row.High, row.Low, row.Close, row.Volume }));
                 linesN.AddRange(valueLinesN);
-                if (Newdata.Count>0)
+                if (Newdata.Count > 0)
                 {
-                    File.WriteAllLines(filedate + "-New-Stock.txt", linesN.ToArray());
+                    File.WriteAllLines("temp\\" + filedate + "-New-Stock.txt", linesN.ToArray());
+                    Console.WriteLine("New stock found, Please update sample.text :- ");
+                    Console.ReadLine();
+                    StockList = File.ReadAllLines("sample.txt")
+                                            .Select(v => getStockList(v))
+                                            .ToList();
+                    SkipList = File.ReadAllLines("skiplist.txt")
+                                            .Select(v => getStockList(v))
+                                            .ToList();
                 }
-                if (Notfounddata.Count>0)
+                if (Notfounddata.Count > 0)
                 {
-                    File.WriteAllLines(filedate + "-NotFound-Stock.txt", Notfounddata.ToArray());
-                }                
+                    File.WriteAllLines("temp\\" + filedate + "-NotFound-Stock.txt", Notfounddata.ToArray());
+                }
                 Newdata = new List<Datamodel>();
                 Notfounddata = new List<string>();
             }
@@ -63,7 +83,7 @@ namespace DailyDataFormat
             Files = d.GetFiles("*NSE-EQ.txt");
             foreach (FileInfo file in Files)
             {
-                List<Datamodel> Stockdata = File.ReadAllLines(file.Name)                                           
+                List<Datamodel> Stockdata = File.ReadAllLines(file.Name)
                                            .Select(v => getBavecopydata(v))
                                            .ToList();
 
@@ -81,79 +101,103 @@ namespace DailyDataFormat
                 var lines = new List<string>();
                 var valueLines = Stockdata.Where(row => row.Name != null).Select(row => string.Join(",", new string[] { row.Name, row.dateDate, row.Open, row.High, row.Low, row.Close, row.Volume }));
                 lines.AddRange(valueLines);
-                File.WriteAllLines(filedate + "-NSE-EQ-F.txt", lines.ToArray());
+                File.WriteAllLines("Output\\" + filedate + "-NSE-EQ-F.txt", lines.ToArray());
 
                 var linesN = new List<string>();
                 var valueLinesN = Newdata.Where(row => row.Name != null).Select(row => string.Join(",", new string[] { row.Name, row.dateDate, row.Open, row.High, row.Low, row.Close, row.Volume }));
                 linesN.AddRange(valueLinesN);
                 if (Newdata.Count > 0)
                 {
-                    File.WriteAllLines(filedate + "-New-Stock.txt", linesN.ToArray());
+                    File.WriteAllLines("temp\\" + filedate + "-New-Stock.txt", linesN.ToArray());
+                    Console.WriteLine("New stock found, Please update sample.text :- ");
+                    Console.ReadLine();
+                    StockList = File.ReadAllLines("sample.txt")
+                                            .Select(v => getStockList(v))
+                                            .ToList();
+                    SkipList = File.ReadAllLines("skiplist.txt")
+                                            .Select(v => getStockList(v))
+                                            .ToList();
                 }
                 if (Notfounddata.Count > 0)
                 {
-                    File.WriteAllLines(filedate + "-NotFound-Stock.txt", Notfounddata.ToArray());
-                }                    
+                    File.WriteAllLines("temp\\" + filedate + "-NotFound-Stock.txt", Notfounddata.ToArray());
+                }
                 Newdata = new List<Datamodel>();
                 Notfounddata = new List<string>();
             }
 
-            
+
         }
 
 
         public static string getStockList(string csvLine)
         {
-            string[] values = csvLine.Split(',');           
-            return values[0];           
+            string[] values = csvLine.Split(',');
+            return values[0];
         }
 
         public static Datamodel getNsedata(string csvLine)
         {
-            Datamodel data = new Datamodel();
-            string[] values = csvLine.Split(',');
-            data.Name = values[0];            
-            DateTime date_Date = Convert.ToDateTime(values[10]);
-            filedate = date_Date.ToString("yyyy-MM-dd");
-            data.dateDate = date_Date.ToString("yyyyMMdd");
-            data.Open = values[2];
-            data.High = values[3];
-            data.Low = values[4];
-            data.Close = values[5];
-            data.Volume = values[8];
-            if (StockList.Contains(values[0]))
+            if (!string.IsNullOrEmpty(csvLine))
             {
-                return data;
+                Datamodel data = new Datamodel();
+                string[] values = csvLine.Split(',');
+                data.Name = values[0];
+                DateTime date_Date = Convert.ToDateTime(values[10]);
+                filedate = date_Date.ToString("yyyy-MM-dd");
+                data.dateDate = date_Date.ToString("yyyyMMdd");
+                data.Open = values[2];
+                data.High = values[3];
+                data.Low = values[4];
+                data.Close = values[5];
+                data.Volume = values[8];
+                if (StockList.Contains(values[0]))
+                {
+                    return data;
+                }
+                else
+                {
+                    if (!SkipList.Contains(values[0]))
+                    {
+                        Newdata.Add(data);
+                    }
+                    
+                    return new Datamodel();
+                }
             }
-            else
-            {
-                Newdata.Add(data);
-                return new Datamodel();
-            }
-            
+
+            return new Datamodel();
+
         }
 
         public static Datamodel getBavecopydata(string csvLine)
         {
-            Datamodel data = new Datamodel();
-            string[] values = csvLine.Split(',');
-            data.Name = values[0];
-            filedate = values[1].Substring(0,4)+"-"+ values[1].Substring(3, 2) + "-"+ values[1].Substring(6, 2);
-            data.dateDate = values[1];
-            data.Open = values[2];
-            data.High = values[3];
-            data.Low = values[4];
-            data.Close = values[5];
-            data.Volume = values[6];
-            if (StockList.Contains(values[0]))
+            if (!string.IsNullOrEmpty(csvLine))
             {
-                return data;
+                Datamodel data = new Datamodel();
+                string[] values = csvLine.Split(',');
+                data.Name = values[0];
+                filedate = values[1].Substring(0, 4) + "-" + values[1].Substring(4, 2) + "-" + values[1].Substring(6, 2);
+                data.dateDate = values[1];
+                data.Open = values[2];
+                data.High = values[3];
+                data.Low = values[4];
+                data.Close = values[5];
+                data.Volume = values[6];
+                if (StockList.Contains(values[0]))
+                {
+                    return data;
+                }
+                else
+                {
+                    if (!SkipList.Contains(values[0]))
+                    {
+                        Newdata.Add(data);
+                    }                   
+                    return new Datamodel();
+                }
             }
-            else
-            {
-                Newdata.Add(data);
-                return new Datamodel();
-            }
+            return new Datamodel();
 
         }
 
